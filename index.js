0400aa1cd14b206d9004a5e9d1003b2bd40b56db
nlarrea <larrea.naia@gmail.com>
2023-03-07 19:20:15 +0100

    error fixed

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var moment = require('moment');
  */
 
 exports.footer = function(name){
-    return "Copyright " + moment().format('YYYY') + " " + name + "All rights reserved.";
+    return "Copyright " + moment().format('YYYY') + " " + name + " All rights reserved.";
 }
